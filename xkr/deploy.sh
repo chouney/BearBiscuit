465e538ad5b8c5e953e5337434e2db556f6716b0
zhangqixiang <chris_zqx@163.com>
2018-06-27 02:23:41 +0800

    MOD:添加环境部署脚本

--- xkr/deploy.sh
+++ xkr/deploy.sh
@@ -3,6 +3,7 @@ profile=dev
 if [ 1 == $# ] ; then
     profile=$1
 fi
+#shutdown
 sh shutdown.sh
 PROJECT_NAME=xkr
 BASE_DIR=/home/admin
@@ -14,4 +15,18 @@ if [ $profile != pro ] ; then
     java_opt=${java_opt}:"-Xdebug -Xrunjdwp,transport=dt_socket,server=y,address=8301,suspend=n,onthrow=java.io.IOException,launch=/sbin/echo"
 fi
 echo ${JAVA_HOME}/bin/java -d64 -jar ${CODE_DIR}/target/xkr.jar $java_opt
-${JAVA_HOME}/bin/java -jar ${CODE_DIR}/target/xkr.jar $java_opt
+nohup ${JAVA_HOME}/bin/java -jar ${CODE_DIR}/target/xkr.jar $java_opt > /dev/null
+
+#check app
+APP_NAME=xkr
+tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+for (( i=1; i<100;i++))
+    do
+        echo 'RUNNING APP HealthCheck!'
+        if [ ${tpid} ]; then
+            echo 'App is running SUCCESS!!!!!!.'
+            break
+        else
+            sleep 1
+    done
+fi
