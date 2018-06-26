74bbcd7ee126336bcc113fcba71d4e0751d0da4a
zhangqixiang <chris_zqx@163.com>
2018-06-27 02:29:11 +0800

    MOD:添加环境部署脚本

--- xkr/deploy.sh
+++ xkr/deploy.sh
@@ -5,11 +5,12 @@ if [ 1 == $# ] ; then
 fi
 #shutdown
 sh shutdown.sh
+
 PROJECT_NAME=xkr
 BASE_DIR=/home/admin
 CODE_DIR=$BASE_DIR/www-logic/$PROJECT_NAME
 cd $CODE_DIR
-#mvn clean install -Dmaven.test.skip=true
+mvn clean install -Dmaven.test.skip=true
 java_opt="--spring.profiles.active=${profile} -Xms256M -Xmx256M -Xmn64M -XX:ErrorFile=${CODE_DIR}/logs/jvm-error-pid.log  -XX:HeapDumpPath=${CODE_DIR}/logs/java_pid.hprof XX:-HeapDumpOnOutOfMemoryError"
 if [ $profile != pro ] ; then
     java_opt=${java_opt}:"-Xdebug -Xrunjdwp,transport=dt_socket,server=y,address=8301,suspend=n,onthrow=java.io.IOException,launch=/sbin/echo"
