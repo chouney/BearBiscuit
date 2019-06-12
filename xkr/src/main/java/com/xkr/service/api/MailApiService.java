f57cbec44f87cb21fc49775c99e43be468836a16
zhangqixiang <chris_zqx@163.com>
2019-06-12 22:47:19 +0800

    FIX： 1.邮箱异步发送邮件

--- xkr/src/main/java/com/xkr/service/api/MailApiService.java
+++ xkr/src/main/java/com/xkr/service/api/MailApiService.java
@@ -2,6 +2,7 @@ package com.xkr.service.api;
 
 import io.github.biezhi.ome.OhMyEmail;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -29,24 +30,24 @@ public class MailApiService {
         OhMyEmail.config(props,"administrator@sharecoder.cn","hellotest520");
     }
 
-    public boolean sendRegValidCaptcha(String email,String userName,String captcha) throws MessagingException {
+    @Async
+    public void sendRegValidCaptcha(String email,String userName,String captcha) throws MessagingException {
         OhMyEmail.subject("sharecoder邮箱验证")
                 .from("sharecoder.cn")
                 .to(email)
 //                .html("您好，您的账号<b>"+userName+"</b>已经注册成功，请您<a href=\""+frontDomain+"/front_pages/checkEmail.html?token=" + captcha + "\">点此链接</a>进行激活操作，没有激活的账号无法正常使用，此链接转发无效。")
                 .html("您好，您的账号<b>"+userName+"</b>已经注册成功，请您点击链接"+frontDomain+"/front_pages/checkEmail.html?token=" + captcha + "进行激活操作，没有激活的账号无法正常使用，此链接转发无效。")
                 .send();
-        return true;
     }
 
-    public boolean sendPasswordUpdateValidCaptcha(String email,String userName,String captcha) throws MessagingException {
+    @Async
+    public void sendPasswordUpdateValidCaptcha(String email,String userName,String captcha) throws MessagingException {
         OhMyEmail.subject("sharecoder邮箱验证")
                 .from("sharecoder.cn")
                 .to(email)
 //                .html("您好，您的账号<b>"+userName+"</b>正在进行修改密码操作，请您<a href=\"" + frontDomain + "/front_pages/checkEmail.html?token=" + captcha + "\">点此链接</a>进行密码修改操作，此链接转发无效。")
                 .html("您好，您的账号<b>"+userName+"</b>正在进行修改密码操作，请您点击链接" + frontDomain + "/front_pages/checkEmail.html?token=" + captcha + "进行密码修改操作，此链接转发无效。")
                 .send();
-        return true;
     }
 
 
