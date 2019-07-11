38315508c39e0a63cab12846d789e31d968158dd
zhangqixiang <chris_zqx@163.com>
2019-07-11 23:12:28 +0800

    FIX： 1.登录顶出

--- xkr/src/main/java/com/xkr/web/filter/AdminLoginShiroFilter.java
+++ xkr/src/main/java/com/xkr/web/filter/AdminLoginShiroFilter.java
@@ -62,21 +62,21 @@ public class AdminLoginShiroFilter extends AdviceFilter {
             return false;
         }
         //检查是否有人登录该账号
-        if(Objects.nonNull(principal.getPrincipal())) {
-            XkrAdminAccount user = (XkrAdminAccount) principal.getPrincipal();
-            if(Objects.nonNull(user)) {
-                XkrLoginToken xkrLoginToken = xkrLoginTokenAgent.getUserLoginRecordById(user.getId());
-                if (Objects.nonNull(xkrLoginToken)) {
-                    String sessionId = xkrLoginToken.getLoginToken();
-                    if (!session.getId().equals(sessionId)) {
-                        httpServletResponse.setCharacterEncoding("UTF-8");
-//            httpServletResponse.setContentType("application/json");
-                        httpServletResponse.getWriter().write(JSON.toJSONString(new BasicResult(ErrorStatus.USER_LOGIN_NEW)));
-                        return false;
-                    }
-                }
-            }
-        }
+//        if(Objects.nonNull(principal.getPrincipal())) {
+//            XkrAdminAccount user = (XkrAdminAccount) principal.getPrincipal();
+//            if(Objects.nonNull(user)) {
+//                XkrLoginToken xkrLoginToken = xkrLoginTokenAgent.getUserLoginRecordById(user.getId());
+//                if (Objects.nonNull(xkrLoginToken)) {
+//                    String sessionId = xkrLoginToken.getLoginToken();
+//                    if (!session.getId().equals(sessionId)) {
+//                        httpServletResponse.setCharacterEncoding("UTF-8");
+////            httpServletResponse.setContentType("application/json");
+//                        httpServletResponse.getWriter().write(JSON.toJSONString(new BasicResult(ErrorStatus.USER_LOGIN_NEW)));
+//                        return false;
+//                    }
+//                }
+//            }
+//        }
         return true;
     }
 
--- xkr/src/main/java/com/xkr/web/filter/UserLoginShiroFilter.java
+++ xkr/src/main/java/com/xkr/web/filter/UserLoginShiroFilter.java
@@ -61,21 +61,21 @@ public class UserLoginShiroFilter extends AdviceFilter {
             return false;
         }
         //检查是否有人登录该账号
-        if(Objects.nonNull(principal.getPrincipal())) {
-            XkrUser user = (XkrUser) principal.getPrincipal();
-            if(Objects.nonNull(user)) {
-                XkrLoginToken xkrLoginToken = xkrLoginTokenAgent.getUserLoginRecordById(user.getId());
-                if (Objects.nonNull(xkrLoginToken)) {
-                    String sessionId = xkrLoginToken.getLoginToken();
-                    if (!session.getId().equals(sessionId)) {
-                        httpServletResponse.setCharacterEncoding("UTF-8");
-//            httpServletResponse.setContentType("application/json");
-                        httpServletResponse.getWriter().write(JSON.toJSONString(new BasicResult(ErrorStatus.USER_LOGIN_NEW)));
-                        return false;
-                    }
-                }
-            }
-        }
+//        if(Objects.nonNull(principal.getPrincipal())) {
+//            XkrUser user = (XkrUser) principal.getPrincipal();
+//            if(Objects.nonNull(user)) {
+//                XkrLoginToken xkrLoginToken = xkrLoginTokenAgent.getUserLoginRecordById(user.getId());
+//                if (Objects.nonNull(xkrLoginToken)) {
+//                    String sessionId = xkrLoginToken.getLoginToken();
+//                    if (!session.getId().equals(sessionId)) {
+//                        httpServletResponse.setCharacterEncoding("UTF-8");
+////            httpServletResponse.setContentType("application/json");
+//                        httpServletResponse.getWriter().write(JSON.toJSONString(new BasicResult(ErrorStatus.USER_LOGIN_NEW)));
+//                        return false;
+//                    }
+//                }
+//            }
+//        }
         return true;
     }
 
