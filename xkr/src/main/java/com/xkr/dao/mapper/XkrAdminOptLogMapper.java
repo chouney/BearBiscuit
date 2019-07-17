ec09bad7fae8aed86d14ff259a2489ca1dfa724b
zhangqixiang <chris_zqx@163.com>
2019-07-18 00:04:58 +0800

    FIX： 1.optlogList

--- xkr/src/main/java/com/xkr/dao/mapper/XkrAdminOptLogMapper.java
+++ xkr/src/main/java/com/xkr/dao/mapper/XkrAdminOptLogMapper.java
@@ -2,6 +2,7 @@ package com.xkr.dao.mapper;
 
 import com.xkr.domain.entity.XkrAdminOptLog;
 import com.xkr.util.CustomerMapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.Map;
 
 @Service
 public interface XkrAdminOptLogMapper extends CustomerMapper<XkrAdminOptLog> {
-    List<XkrAdminOptLog> getAllOptLogByAdminAccount(Long adminAccountId);
+    List<XkrAdminOptLog> getAllOptLogByAdminAccount(@Param("adminAccountId") Long adminAccountId);
 
     Integer batchUpdateOptLogByIds(Map<String,Object> params);
 }
